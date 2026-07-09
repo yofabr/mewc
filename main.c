30c9ab326e19b3f758968fca6926c27e7cb28d45
Yoftahe <jofab08@gmail.com>
2026-07-09 19:23:00 +0300

    chore: Open and read files]

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
+#include <bits/pthreadtypes.h>
 #include <stdio.h>
 
 
 char* get_file_content(char* file_path) {
+
+  FILE *fptr;
+
+  fptr = fopen(file_path, "r");
+  if(fptr == NULL) {
+    printf("Unable to open file!");
+    return "";
+  }
+
+  printf("fILE CONTENT: %s", fptr);
   return "";
 }
 
@@ -14,5 +25,6 @@ int main(int argc, char* argv[]) {
   if(path == NULL) {
     printf("ERROR: File path was not provided");
   }
+  get_file_content(path);
   return 0;
 }
